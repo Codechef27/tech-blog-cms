bc6550a0558efebc044580c13bd344b1dfdd3a18
BradleyK <bradleyjkennett@gmail.com>
2022-07-15 12:41:50 -0400

    user routes all & one add

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes.js');
+// const blogPostRoutes = require('./blog-post-routes');
+// const commentRoutes = require('./comment-routes');
+
+router.use('/users', userRoutes);
+// router.use('/blog-post', blogPostRoutes);
+// router.use('/comments', commentRoutes);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-// const routes = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-// const path = require('path');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,22 +10,22 @@ const PORT = process.env.PORT || 3001;
 // const exphbars = require('express-handlebars');
 // const hbars = exphbars.create({ helpers });
 
-// const session = require('express-session');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-// app.use(session(sess));
+app.use(session(sess));
 
 // app.engine('handlebars', hbars.engine);
 // app.set('view engine', 'handlebars');
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
-// app.use(routes);
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
